9dd87243585ca5dff3b67fb579344804682e3d81
Chris Devine <chris.devine89@gmail.com>
2017-05-04 15:34:40 +0100

    Modifying Data via an API

--- MyCodeCamp/Controllers/CampsController.cs
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -1,14 +1,126 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
 
 namespace MyCodeCamp.Controllers
 {
     [Route("api/[controller]")]
     public class CampsController : Controller
     {
+        private ICampRepository _repo;
+        private ILogger<CampsController> _logger;
+
+        public CampsController(ICampRepository repo, ILogger<CampsController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
         [HttpGet("")]
         public IActionResult Get()
         {
-            return Ok(new {Name = "Shawn", FavoriteColor = "Blue"});
+            var camps = _repo.GetAllCamps();
+
+            return Ok(camps);
+        }
+
+        // If the param is not part of the attribute it will become a querystring and if given a default here will be optional in the web address
+        [HttpGet("{id}", Name = "CampGet")]
+        public IActionResult Get(int id, bool includeSpeakers = false)
+        {
+            try
+            {
+                Camp camp = null;
+
+                if (includeSpeakers) camp = _repo.GetCampWithSpeakers(id);
+                else camp = _repo.GetCamp(id);
+
+                if (camp == null) return NotFound($"Camp {id} was not found");
+
+                return Ok(camp);
+            }
+            catch
+            {
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Camp model)
+        {
+            try
+            {
+                _logger.LogInformation("Creating a new code camp");
+
+                _repo.Add(model);
+                if (await _repo.SaveAllAsync())
+                {
+                    var newUri = Url.Link("CampGet", new {id = model.Id});
+                    // model will now contain an id because its been saved to the DB
+                    return Created(newUri, model);
+                }
+                else
+                {
+                    _logger.LogWarning("Could not save camp to the database");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Threw exception while saving camp: {ex}");
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("id")]
+        public async Task<IActionResult> Put(int id, [FromBody]Camp model)
+        {
+            try
+            {
+                var oldCamp = _repo.GetCamp(id);
+                if (oldCamp == null) return NotFound($"Could not find a camp with the ID of {id}");
+
+                // Map model to the oldcamp
+
+                oldCamp.Name = model.Name ?? oldCamp.Name;
+                oldCamp.Description = model.Description ?? oldCamp.Description;
+                oldCamp.Location = model.Location ?? oldCamp.Location;
+                oldCamp.Length = model.Length > 0 ? model.Length : oldCamp.Length;
+                oldCamp.EventDate = model.EventDate != DateTime.MinValue ? model.EventDate : oldCamp.EventDate;
+
+
+                if (await _repo.SaveAllAsync())
+                {
+                    return Ok(oldCamp);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+
+            return BadRequest("Couldn't update Camp");
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var oldCamp = _repo.GetCamp(id);
+                if (oldCamp == null) return NotFound($"Could not find camp with ID if {id}");
+
+                _repo.Delete(oldCamp);
+
+                if (await _repo.SaveAllAsync())
+                    return Ok();
+            }
+            catch (Exception)
+            {
+            }
+
+            return BadRequest("Couldn't update Camp");
         }
     }
 }
--- MyCodeCamp/Startup.cs
+++ MyCodeCamp/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MyCodeCamp.Data;
+using Newtonsoft.Json;
 
 namespace MyCodeCamp
 {
@@ -29,17 +31,34 @@ namespace MyCodeCamp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(_config);
-            // Add framework services.
-            services.AddMvc();
+            services.AddDbContext<CampContext>(ServiceLifetime.Scoped);
+            services.AddScoped<ICampRepository, CampRepository>();
+            services.AddTransient<CampDbInitializer>();
+
+            // Add framework services. Also stops circular ref's
+            services.AddMvc()
+                .AddJsonOptions(opt =>
+                {
+                    opt.SerializerSettings.ReferenceLoopHandling =
+                        ReferenceLoopHandling.Ignore;
+                });
         } 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            CampDbInitializer seeder)
         {
             loggerFactory.AddConsole(_config.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseMvc();
+            app.UseMvc(config =>
+            {
+                
+            });
+
+            seeder.Seed().Wait();
         }
     }
 }
